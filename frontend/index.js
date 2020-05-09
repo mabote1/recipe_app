9ceb6c8e2c1165be687a07599678a5dcd852b9ec
Anders <sredna43@gmail.com>
2020-05-09 01:32:57 -0500

    updated frontend example in index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,56 @@
 // Example query to the backend server:
-/*
-fetch('/graphql', {
+const fetch = require('node-fetch')
+
+/* Example using hard coded data -- good for querying names */
+fetch('http://localhost:4000/graphql', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     },
-    body: JSON.stringify({query: "{ hello }"")
+    body: JSON.stringify({query: `
+    {
+        hello
+        names{
+            recipe_id
+            name
+        } 
+    }
+    `})
 })
     .then(r => r.json())
-    .then(data => console.log('Data returned: ', data));
-*/
+    .then(data => console.log('Data returned: ', data.data));
+
+
+
+/* Example using a query and variables -- good for querying recipes by recipe_id */
+let query = `query Example($id: Int!) {
+    recipe(id:$id) {
+        name
+        description
+        author
+        ingredients {
+          name
+          amount
+          measurement
+          category
+        }
+        category
+        serves
+      }
+}`
+let id = 1;
+
+fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+        query,
+        variables: { id },
+    })
+})
+    .then(r => r.json())
+    .then(data => console.log('Data returned: ', data.data.recipe.ingredients));
