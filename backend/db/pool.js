afa850fe3b33c048c91e0e13bee7295ffb4b0284
Thabiso Mabote <mabote1@stolaf.edu>
2020-05-11 00:49:06 -0500

    Updating pool.js for .pgpass to work on any windows machine as suggested by rab

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,20 +1,26 @@
 const { Pool } = require('pg');
 const fs = require('fs');
-try {
-    var pgpass = fs.readFileSync('db/.pwd');
-} catch (error) {
-    var pgpass = fs.readFileSync('.pwd');
-}
 
-var username = 'olson16';
+const lib = require('./mcalib');
+lib.setErrorPrefix(__filename);  // set label for lib error messages
+
+// database connection parameters
+const dbHost = "csinparallel.cs.stolaf.edu";
+const username = 'mabote1';    // CHANGE to your username, e.g., jones1
+const password = lib.getPGPassword(dbHost);  // uncomment for Windows
+const dbName = 'mca_s20';
+const schema = 'mabote1';  // CHANGE to your username as schema for Lab 5
+                       // CHANGE to team schema for project
 
 const pool = new Pool({
     user: username,
-    host: 'csinparallel.cs.stolaf.edu',
-    database: 'mca_s20',
-    password: pgpass,
+    password: password,                      // uncomment for Windows
+    host: dbHost,
+    database: dbName,
     port: 5432,
 });
+
+
 
 var pgschema = 'mca_s20_recipe, olson16'
 
