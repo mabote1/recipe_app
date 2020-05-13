21fabf56b848ac148978ed8dae7293bd4fd15f80
Anders <sredna43@gmail.com>
2020-05-13 10:14:08 -0500

    using .env

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,38 +1,16 @@
 const { Pool } = require('pg');
 const fs = require('fs');
-const prompt = require('prompt-sync')();
-
-const lib = require('./mcalib');
-lib.setErrorPrefix(__filename);  // set label for lib error messages
-
-// database connection parameters
-const dbHost = "csinparallel.cs.stolaf.edu";
+require('dotenv').config();
 
 console.log(`Hello! Running in ${process.env.NODE_ENV} mode.`)
 
-var username = '';
-
-if(process.env.NODE_ENV === "prod"){
-    username = prompt('Enter your DB Username: ') || 'mabote1';
-}
-else {
-    username = 'mabote1'
-}
+// database connection parameters
+const dbHost = process.env.DB_HOST;
+const username = process.env.DB_USER;
 console.log(`Username set to ${username}`);
-
-var password = '';
-try {
-    password = lib.getPGPassword(dbHost);  // uncomment for Windows
-} catch {
-    password = fs.readFileSync('db/.pwd', {encoding:'utf8'});
-}
-
-password = password.trim();
-
+const password = fs.readFileSync('db/.pwd', {encoding:'utf8'}).trim();
 console.log(`Password set to ${password.substr(0,4)}********************`);
-
-const dbName = 'mca_s20';
-
+const dbName = process.env.DB_NAME;
 const pool = new Pool({
     user: username,
     password: password,                      // uncomment for Windows
@@ -41,17 +19,14 @@ const pool = new Pool({
     port: 5432,
 });
 
-
-
-var pgschema = 'mca_s20_recipe, olson16'
-
 pool.on('error', (err, client) => {
     console.error('Error on idle client', err)
     process.exit(-1)
 })
 
+const pgschema = `SET search_path = ${process.env.DB_SCHEMA}, ${username}, public;`;
 pool.on('connect', client => {
-    client.query(`SET search_path = ${pgschema}, public;`)
+    client.query(pgschema)
 })
 
 module.exports = pool;
