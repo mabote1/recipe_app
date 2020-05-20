2b566c6e3f33cbc88e10e930affa5847d24322cd
Thabiso Mabote <mabote1@stolaf.edu>
2020-05-19 22:19:47 -0500

    Moved the frontend of all recipes into frontend/ and updated the pool for its backend

--- backend/recipe-REStful-db/app.js
+++ backend/recipe-REStful-db/app.js
@@ -91,6 +91,55 @@ lib.setErrorPrefix(__filename);  // set label for lib error messages
 
 // database connection parameters
 const dbHost = "csinparallel.cs.stolaf.edu";
+
+console.log(`Hello! Running in ${process.env.NODE_ENV} mode.`)
+
+var username = '';
+
+if(process.env.NODE_ENV === "prod"){
+    username = prompt('Enter your DB Username: ') || 'mabote1';
+}
+else {
+    username = 'mabote1'
+}
+console.log(`Username set to ${username}`);
+
+var password = '';
+try {
+    password = lib.getPGPassword(dbHost);  // uncomment for Windows
+} catch {
+    password = fs.readFileSync('db/.pwd', {encoding:'utf8'});
+}
+
+password = password.trim();
+
+console.log(`Password set to ${password.substr(0,4)}********************`);
+
+const dbName = 'mca_s20';
+
+const pool = new Pool({
+    user: username,
+    password: password,                      // uncomment for Windows
+    host: dbHost,
+    database: dbName,
+    port: 5432,
+});
+
+
+
+var pgschema = 'mca_s20_recipe, olson16'
+
+pool.on('error', (err, client) => {
+    console.error('Error on idle client', err)
+    process.exit(-1)
+})
+
+pool.on('connect', client => {
+    client.query(`SET search_path = ${pgschema}, public;`)
+})
+
+/*// database connection parameters
+const dbHost = "csinparallel.cs.stolaf.edu";
 const user = 'mabote1';    // CHANGE to your username, e.g., jones1
 const password = lib.getPGPassword(dbHost);  // uncomment for Windows
 const dbName = 'mca_s20';
@@ -114,7 +163,7 @@ pool.on('connect', client => {
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err)
   process.exit(-1)
-})
+})*/
 
 console.log(`Connected to database ${dbName} on ${dbHost}`);
 
